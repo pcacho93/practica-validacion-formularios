0584163f839012e4ddac4bba8fe0ba5625c3a4fb
pcacho <pcacho93@gmail.com>
2015-02-06 13:03:54 +0100

    Archivos cambiados para funcionar en vesta

--- app/php/cp.php
+++ app/php/cp.php
@@ -2,9 +2,9 @@
 /* Descomentaríamos la siguiente línea para mostrar errores de php en el fichero: */
 // ini_set('display_errors', '1');
 /* Definimos los parámetros de conexión con la bbdd: */
-$dbinfo = "mysql:dbname=formulario;host=localhost";
-$user = "root";
-$pass = "root";
+$dbinfo = "mysql:dbname=pablocacho_formulario;host=localhost";
+$user = "pablocacho_a";
+$pass = "123456";
 //Nos intentamos conectar:
 try {
     /* conectamos con bbdd e inicializamos conexión como UTF8 */
